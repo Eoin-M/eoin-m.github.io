2ca49885150afe9a166642256b1b58617351ebbb
Eoin Maguire <eoin.mgr@gmail.com>
2016-03-19 03:50:39 +0000

    Dropdown menu now collapses on link click on mobile

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,20 @@
 $(document).ready(function(){
 	$("#myNavbar a").on('click', function(event){
 
-	  // Prevent default anchor click behavior
-	  event.preventDefault();
+		// Prevent default anchor click behavior
+		event.preventDefault();
 
-	  // Store hash (#)
-	  var hash = this.hash;
+		// Store hash (#)
+		var hash = this.hash;
 
-	  // Using jQuery's animate() method to add smooth page scroll
-	  $('html, body').animate({
-		scrollTop: $(hash).offset().top
-	  }, 600);
+		// Using jQuery's animate() method to add smooth page scroll
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 600);
 	});
+	
+	//Collapse the navar list when one is clicked
+	$(".navbar-nav li a").click(function(event) {
+		$(".navbar-collapse").collapse('hide');
+	});  
 });
